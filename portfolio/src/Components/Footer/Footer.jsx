4d6881eb9729e697960390d43766ea2fc6c8f180
dizas9 <sazid.ice07@gmail.com>
2024-01-16 22:25:02 +0600

    Production Review 1

--- portfolio/src/Components/Footer/Footer.jsx
+++ portfolio/src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
           className="w-7 rounded-full"
           initial={{ rotate: 0 }}
           animate={{ rotate: [0, 30, 60, 90, 180, 360] }}
-          transition={{duration:0.5, repeat:Infinity ,type:"tween"}}
+          transition={{ duration: 0.5, repeat: Infinity, type: "tween" }}
         />
         <span className="font-bold">by Sazid (diza9)</span>
       </p>
