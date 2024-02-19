88aa4b6afbf27099e2d06846715e3f121ca124d2
dizas9 <sazid.ice07@gmail.com>
2024-02-19 20:51:16 +0600

    About Section update

--- portfolio/src/Components/About Section/About.jsx
+++ portfolio/src/Components/About Section/About.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
-import { motion, AnimatePresence, delay } from "framer-motion";
-import { infodata, currentLearning, guidingPrincipl } from "./Infodata";
+// import { useState, useEffect } from "react";
+import { motion} from "framer-motion";
+import { infodata} from "./Infodata";
 import { useScroll } from "../../hooks/useScroll";
+import AccordionCustomIcon from "./AccordionCustomIcon";
 export default function About({ title, id }) {
   const { scrollY } = useScroll(0);
   console.log("sc", scrollY);
@@ -13,9 +14,9 @@ export default function About({ title, id }) {
   return (
     <div
       id={id}
-      className={`h-screen absolute w-screen top-[90vh] flex flex-col justify-center items-center`}
+      className={`h-fit absolute w-screen top-[90vh] flex flex-col justify-center items-center`}
     >
-      {scrollY >= 424 && (
+      {scrollY >= 434 && scrollY<=1000 && (
         <motion.p
           className="text-center text-highlightDark pb-10 font-bold text-2xl lg:text-3xl"
           initial={{ y: 500 }}
@@ -35,40 +36,47 @@ export default function About({ title, id }) {
           <p className="text-justify px-5 text-lite font-thin lg:text-2xl">
             {infodata.info}
           </p>
-          <div className="text-justify px-5 text-lite lg:text-2xl">
-            {/* <details className="pt-2 lg:pt-4">
-          <summary className="font-bold text-highlightDark">
-            Things I Currently Learning -
-          </summary>
-          <ul>
-            {currentLearning.map((item, index) => {
-              return (
-                <p className="pt-4 font-thin" key={index}>
-                  <span className="font-semibold">{item.title}</span>{" "}
-                  {item.list}
-                </p>
-              );
-            })}
-          </ul>
-        </details>
-        <details className="pt-2 lg:pt-4">
-          <summary className="font-bold text-highlightDark">
-            My guiding principles involve -
-          </summary>
-          <ul>
-            {guidingPrincipl.map((item, index) => {
-              return (
-                <p className="pt-4 font-thin" key={index}>
-                  <span className="font-semibold">{item.title}</span>{" "}
-                  {item.info}
-                </p>
-              );
-            })}
-          </ul>
-        </details> */}
-          </div>
+
+          {/* //accordian */}
+
+          <AccordionCustomIcon />
         </motion.div>
       )}
     </div>
   );
 }
+
+
+
+        //   <div className="text-justify px-5 text-lite lg:text-2xl">
+        //     {/* <details className="pt-2 lg:pt-4">
+        //   <summary className="font-bold text-highlightDark">
+        //     Things I Currently Learning -
+        //   </summary>
+        //   <ul>
+        //     {currentLearning.map((item, index) => {
+        //       return (
+        //         <p className="pt-4 font-thin" key={index}>
+        //           <span className="font-semibold">{item.title}</span>{" "}
+        //           {item.list}
+        //         </p>
+        //       );
+        //     })}
+        //   </ul>
+        // </details>
+        // <details className="pt-2 lg:pt-4">
+        //   <summary className="font-bold text-highlightDark">
+        //     My guiding principles involve -
+        //   </summary>
+        //   <ul>
+        //     {guidingPrincipl.map((item, index) => {
+        //       return (
+        //         <p className="pt-4 font-thin" key={index}>
+        //           <span className="font-semibold">{item.title}</span>{" "}
+        //           {item.info}
+        //         </p>
+        //       );
+        //     })}
+        //   </ul>
+        // </details> */}
+        //   </div>;
